c1efe57bd0f78f1d4327de9e567342d308fafc90
Donggyu <a29661498@gmail.com>
2022-09-07 07:31:24 +0900

    feat: 1장 예제 코드 작성

---
+++ src/main/java/Customer.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+    private String name;
+    private List<Rental> rentals = new ArrayList<>();
+
+    public Customer(final String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+
+        String result = "Rental Record for " + getName() + "\n";
+
+        for (Rental rental : rentals) {
+            double thisAmount = 0;
+
+            switch (rental.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (rental.getDaysRented() > 2) {
+                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += rental.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDREN:
+                    thisAmount += 1.5;
+                    if (rental.getDaysRented() > 3) {
+                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                    }
+                    break;
+            }
+
+            frequentRenterPoints ++;
+
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) {
+                frequentRenterPoints ++;
+            }
+
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            totalAmount += thisAmount;
+        }
+
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + "frequent renter points";
+
+        return result;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/Rental.java
@@ -0,0 +1,18 @@
+public class Rental {
+
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(final Movie movie, final int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+}
