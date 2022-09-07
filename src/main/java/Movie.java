b2959a8a7314cd57acfd6a1272ff8d0b2e4c0da3
Donggyu <a29661498@gmail.com>
2022-09-07 09:12:55 +0900

    refactor: 자기 자신의 데이터를 사용하도록 수정

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,8 +1,8 @@
 public class Movie {
 
-    public static final int CHILDREN = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
+    private static final int CHILDREN = 2;
+    private static final int REGULAR = 0;
+    private static final int NEW_RELEASE = 1;
 
     private String title;
     private int priceCode;
@@ -14,6 +14,36 @@ public class Movie {
 
     public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
+    }
+
+    public double getCharge(final int daysRented) {
+        double result = 0;
+
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(final int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        }
+        return 1;
     }
 
     public String getTitle() {
